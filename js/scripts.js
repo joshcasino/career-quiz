03a4ec7ed41cb1db7c9ec7bf2f40a3c9214ee14b
Josh Casuccio <joshcasino@gmail.com>
2016-08-20 15:10:19 -0700

    corrects results displaying multiple time on same page. Also cleaning of indentations and code organization.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("form#carPath").submit(function(event) {
+
     var name = $("#nameQuizForm").val();
     var style = parseInt($("input:radio[name=style]:checked").val());
     var environment = parseInt($("input:radio[name=environment]:checked").val());
@@ -8,33 +9,59 @@ $(document).ready(function() {
     var excited = parseInt($("input:radio[name=excited]:checked").val());
     var score = parseInt(style + environment + programming + logic + excited);
 
+
     $("#nameDisplay").text(name);
 
     if (!score) {
       alert("Please enter all choices");
-    }  else if (score < 14) {
-        $("#ruby").show();
-      } else if (score > 14 && score < 18){
-          $("#php").show();
-        } else if (score > 18 && score < 23){
-            $("#cSharp").show();
-          }else if(score > 23 && score < 28){
-              $("#css").show();
-            } else {
-                $("#java").show();
-              }
+    }
+    else if (score < 14) {
+      $("#ruby").show();
+      $("#php").hide();
+      $("#java").hide();
+      $("#cSharp").hide();
+      $("#css").hide();
+    }
+    else if (score > 14 && score < 18){
+      $("#php").show();
+      $("#ruby").hide();
+      $("#java").hide();
+      $("#cSharp").hide();
+      $("#css").hide();
+    }
+    else if (score > 18 && score < 23){
+      $("#cSharp").show();
+      $("#php").hide();
+      $("#java").hide();
+      $("#ruby").hide();
+      $("#css").hide();
+    }
+    else if(score > 23 && score < 28){
+      $("#css").show();
+      $("#php").hide();
+      $("#java").hide();
+      $("#cSharp").hide();
+      $("#ruby").hide();
+            }
+    else {
+      $("#java").show();
+      $("#php").hide();
+      $("#ruby").hide();
+      $("#cSharp").hide();
+      $("#css").hide();
+    }
 
     event.preventDefault();
 
     });
 
-    $("#resetBtn").click(function() {
-      $("#ruby").hide();
-      $("#php").hide();
-      $("#cSharp").hide();
-      $("#css").hide();
-      $("#java").hide();
-    		$("#ruby#php#cSharp#java#css").hide();
-    		$(".container").css("background-color", "white");
-    	});
+  $("#resetBtn").click(function() {
+    $("#ruby").hide();
+    $("#php").hide();
+    $("#cSharp").hide();
+    $("#css").hide();
+    $("#java").hide();
+  	$("#ruby#php#cSharp#java#css").hide();
+  	$(".container").css("background-color", "white");
+	});
 });
